da2b4677bd337315b322eab17d84710a80522857
Marco Campana <marco@migcan.com>
2009-07-30 17:42:09 +0100

    Demonized script

--- git_notifier_daemon.rb
+++ git_notifier_daemon.rb
@@ -4,35 +4,39 @@
 # 1) Error handling with notifications
 # 2) demonization
 # 3) Create gem
-
-
-
+# 4) check dependencies at startup
 
 require 'rubygems'
 require 'daemons'
 require 'ruby-growl'
 require 'digest/sha1'
 
+# TODO remove this
+require 'ruby-debug'
+
 # Initialize the app while we're not a daemon
 def init()
-  uri   = ARGV[0]
-  @@branch = ARGV[1] || 'master'
+  uri   = ARGV[1]
+  @@branch = ARGV[2] || 'master'
 
   @@repo_path = "/var/tmp/git_#{Digest::SHA1.hexdigest(uri)}_#{@@branch}"
   if !File.exists?( @@repo_path )
-    `mkdir #{@@repo_path}; cd #{@@repo_path}; git clone #{uri} . >/dev/null 2>&1`
+    puts "Cloning repo to watch..."
+    `mkdir #{@@repo_path}; cd #{@@repo_path}; git clone #{uri} .`
   end
 end
 
 init()
 
-# Daemons.run_proc('git_notifier') do
+# TODO When creating the gem, change the pid directory to pids/
+Daemons.run_proc('git_notifier', :dir_mode => :normal, :dir => '/var/tmp' ) do
   g = Growl.new "localhost", "ruby-growl", ["git_notifier"]
   g.notify "git_notifier", "GIT Notifier", "Start watching\nrepo: #{ARGV[0]}\nbranch: #{@@branch}"
 
   loop do
     git_pull_output = `cd #{@@repo_path}; git checkout -b #{@@branch} >/dev/null 2>&1; git pull origin #{@@branch} 2>/dev/null`
 
+    # TODO improve this, It doesn't work all the times
     if git_pull_output =~ /Updating (.+)\.\.(.+)/
       from_commit = $1
       to_commit   = $2
@@ -47,7 +51,8 @@ init()
       # TODO truncate commit sha1 and author name stripping off email
 
       g.notify "git_notifier", sha1 + date, author + "\n" + body
+      # TODO increase sleep time
       sleep( 60 )
     end
   end
-# end
+end
