1f4d6b2e607091a327f91b8f0051b4147b4931f5
Marco Campana <marco@migcan.com>
2009-10-11 19:42:38 +0100

    Added support for watching multiple repositories
    - Completed usage section in the rdoc
    - Completed gemspec file for gemification

--- lib/git-notifier/git-notifier.rb
+++ lib/git-notifier/git-notifier.rb
@@ -3,27 +3,28 @@
 #   This is a sample description of the application.
 #   Blah blah blah.
 #
-# == Examples
-#   This command does blah blah blah.
-#     ruby_cl_skeleton foo.txt
-#
-#   Other examples:
-#     ruby_cl_skeleton -q bar.doc
-#     ruby_cl_skeleton --verbose foo.html
-#
 # == Usage
-#   git-notifier start uri
+#   git-notifier start|stop|add|clear|status
 #
+#   Start the notifier:
+#     git-notifier start
+# 
+#   Stop the notifier:
+#     git-notifier stop
 #
-# == Options
-#   -h, --help          Displays help message
-#   -v, --version       Display the version, then exit
-#   -q, --quiet         Output as little as possible, overrides verbose
-#   -V, --verbose       Verbose output
-#   TO DO - add additional options
-#
+#   Add a repository to the watch list:
+#     git-notifier add <repo_uri> <branch>
+#     Example: git-notifier add git@github.com:marcocampana/git-notifier.git master
+# 
+#   Remove all watched repositories from the watch list
+#     git-notifier clear
+# 
+#   Show all the watched repositories
+#     git-notifier status
+# 
 # == Author
 #   Marco Campana <m.campana@gmail.com>
+#   http://xterm.it/blog
 #
 # == Copyright
 #   Copyright (c) 2009 Marco Campana. Licensed under the MIT License:
@@ -43,56 +44,112 @@ require 'rdoc/usage'
 # TODO remove this
 require 'ruby-debug'
 
-# Initialize the app while we're not a daemon
-def init()
-  repo_uri    = ARGV[1]
-  @@branch    = ARGV[2] || 'master'
-  @@repo_path = "/var/tmp/git_#{Digest::SHA1.hexdigest(repo_uri)}_#{@@branch}"
+def add_repo
+  (RDoc::usage('usage') and return) if ARGV.size < 2
+  repo_uri        = ARGV[1]
+  branch          = ARGV[2] || 'master'
+  repo_name       = repo_uri.split("/").last
+  repo_local_path = "/var/tmp/git-notifier_#{Digest::SHA1.hexdigest(repo_uri)}_#{repo_name}_#{branch}"
+  # TODO Truncate the SHA1 in the filename
 
-  if !File.exists?( @@repo_path )
-    puts "Starting up notifier... (this might take a while)"
-    `mkdir #{@@repo_path}; cd #{@@repo_path}; git clone #{uri} .`
+  if !File.exists?( repo_local_path )
+    puts "git-notifier: adding #{repo_uri} to watch list... (this might take a while)"
+    output = `mkdir #{repo_local_path}; cd #{repo_local_path}; git clone #{repo_uri} . 2>/dev/null`
+    # TODO Add error handling in case the repo does not exist
   end
-  puts "git-notifier is now active"
-rescue
-  RDoc::usage('usage')
+  puts "git-notifier: repo '#{repo_uri}' added to watch list"
 end
 
-def stop()
+def start
+  demonize
+end
+
+def stop
+  demonize
   puts "git-notifier is now stopped"
 end
 
-def clear()
+def status
+  watched_repos = []
+  available_repos.each do |repo_dirname|
+    prefix, repo_sha1, repo_name, repo_branch = repo_dirname.split('_')
+    watched_repos << [repo_name, repo_branch]
+  end
+  if watched_repos.any?
+    # TODO Show if the notifier is running or not
+    puts "git-notifier: the following repositories are in the watch list"
+    watched_repos.each do |repo|
+      puts " - #{repo[0]} (#{repo[1]})"
+    end
+  else
+    puts "git-notifier: no repositories are being watched at the moment\nUse: 'git-notifier add <repo_uri>' to add repos to watch"
+  end
 end
 
-init() if ARGV[0] == 'start'
-stop() if ARGV[0] == 'stop'
-clear() if ARGV[0] == 'clear'
+def available_repos
+  repos = []
+  Dir.new('/var/tmp').each do |repo_dir|
+    repos << repo_dir if repo_dir =~ /^git-notifier/ 
+  end
+  repos
+end
 
-Daemons.run_proc('git_notifier', :dir_mode => :normal, :dir => 'pids/' ) do
-  g = Growl.new "localhost", "ruby-growl", ["git_notifier"]
-  g.notify "git_notifier", "GIT Notifier", "Start watching\nrepo: #{ARGV[0]}\nbranch: #{@@branch}"
+def clear_all
+  repos = available_repos
+  repos.each do |repo|
+    puts "/var/tmp/#{repo} deleted"
+    `cd /var/tmp; rm -Rf #{repo}` if repo =~ /^git-notifier/
+  end
+  puts "git-notifier: no repositories are being watched at the moment\nUse: 'git-notifier add <repo_uri>' to add repos to watch" if repos.empty?
+end
 
-  loop do
-    git_pull_output = `cd #{@@repo_path}; git checkout -b #{@@branch} >/dev/null 2>&1; git pull origin #{@@branch} 2>/dev/null`
+def demonize
+  Daemons.run_proc('git-notifier', :dir_mode => :normal, :dir => 'pids/', :monitor => false) do
+    g = Growl.new "localhost", "ruby-growl", ["git-notifier"]
+    # g.notify "git-notifier", "GIT Notifier", "Start watching\nrepo: #{ARGV[0]}\nbranch: #{@@branch}"
+    g.notify "git-notifier", "GIT Notifier", "Start notifier"
 
-    # TODO improve this, It doesn't work all the times
-    if git_pull_output =~ /Updating (.+)\.\.(.+)/
-      from_commit = $1
-      to_commit   = $2
+    loop do
+      # TODO store available_repoes in a variable if adding while running doesn't work
+      available_repos.each do |repo_dirname|
+        prefix, repo_sha1, repo_name, repo_branch = repo_dirname.split('_')
+        
+        git_pull_output = `cd /var/tmp/#{repo_dirname}; git checkout -b #{repo_branch} >/dev/null 2>&1; git pull origin #{repo_branch} 2>/dev/null`
 
-      git_log_output = `cd #{@@repo_path}; git log -1 $1`
+        if git_pull_output =~ /Updating (.+)\.\.(.+)/
+          from_commit = $1
+          to_commit   = $2
 
-      notification_lines = git_log_output.split( "\n" )
-      sha1 = notification_lines[ 0 ]
-      author = notification_lines[ 1 ]
-      date = notification_lines[ 2 ]
-      body = notification_lines[ 3 .. notification_lines.size - 1 ].to_s.gsub( /\t/, '')
-      # TODO truncate commit sha1 and author name stripping off email
+          git_log_output = `cd /var/tmp/#{repo_dirname}; git log -1 $1`
 
-      g.notify "git_notifier", sha1 + date, author + "\n" + body
-      # TODO increase sleep time
-      sleep( 60 )
+          notification_lines = git_log_output.split( "\n" )
+          sha1 = notification_lines[0]
+          author = notification_lines[1]
+          date = notification_lines[2]
+          body = notification_lines[3 .. notification_lines.size - 1].to_s.gsub( /\t/, '')
+          # TODO truncate commit sha1 and author name stripping off email
+
+          g.notify "git-notifier", sha1 + date, author + "\n" + body
+        end
+        # TODO increase sleep time
+        sleep( 60 )
+      end
     end
   end
 end
+
+case ARGV[0]
+  when 'start'
+    start
+  when 'add'
+    add_repo
+  when 'stop'
+    stop
+  when 'clear'
+    clear_all
+  when 'status'
+    status
+  else
+    RDoc::usage('usage')
+end
+
